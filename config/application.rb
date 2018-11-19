dcab2025067b52940c0b177521136be52e5e219f
rosemberg-al <rosemberg_oliveira@hotmail.com>
2018-11-19 16:30:44 -0300

    criado o arquivo com dados de senha do banco, para separar do git

--- config/application.rb
+++ config/application.rb
@@ -22,10 +22,20 @@ module Lifeproject
     config.time_zone = 'Brasilia'
     config.i18n.available_locales = ["en", "pt-BR"]
 
-  
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+
+    #configuration for the database and other info
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'application.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+    
   end
 end
